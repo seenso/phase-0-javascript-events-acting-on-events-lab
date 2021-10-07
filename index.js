a2a5a212c71fa509c824fea93da5b903ce2de035
seenso <>
2021-10-07 13:10:47 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 // Your code here
 const dodger = document.getElementById("dodger");
-//dodger is 40px wide per the css file
 
 function moveDodgerLeft() {
   const leftNumbers = dodger.style.left.replace("px", "");
   const left = parseInt(leftNumbers, 10);
-
+  
   if (left > 0) {
     dodger.style.left = `${left - 1}px`;
   }
@@ -13,17 +12,15 @@ function moveDodgerLeft() {
 
 //want to keep using the left property to move the dodger right.
 function moveDodgerRight() {
-    const leftNumbers = dodger.style.left.replace("px", "");
+  const leftNumbers = dodger.style.left.replace("px", "");
   const left = parseInt(leftNumbers, 10);
-
-  if (left > 0) {
+  
+  //dodger is 40px wide per the css file
+  //the game is 400px wide
+  if (left < 360) {
     dodger.style.left = `${left + 1}px`;
   }
 };
-
-
-
-
 
 document.addEventListener("keydown", function (event) {
   if (event.key === 'ArrowLeft') {
