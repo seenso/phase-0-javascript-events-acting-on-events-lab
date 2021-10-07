2ebbe84e50cfb68a1c0909bcb5bef1406a9fc577
seenso <>
2021-10-07 11:50:38 -0800

    Passed test for moveDodgerLeft()

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 // Your code here
-function moveDodgerLeft() {};
+const dodger = document.getElementById("dodger");
+//dodger is 40px wide per the css file
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+}
 
 function moveDodgerRight() {};
