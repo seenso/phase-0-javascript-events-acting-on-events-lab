d92b3f5abd18ba6c70d0ab9004e82f7673290e71
seenso <>
2021-10-07 13:07:27 -0800

    Fixed moveDodgerleft(). Created addEventListener for left and right keys. Created moveDodgerRight() but still need to ensure it doesn't go over the edge.

--- index.js
+++ index.js
@@ -9,12 +9,26 @@ function moveDodgerLeft() {
   if (left > 0) {
     dodger.style.left = `${left - 1}px`;
   }
+};
 
-  document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowLeft") {
-      moveDodgerLeft();
-    }
-  });
-}
+//want to keep using the left property to move the dodger right.
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
 
-function moveDodgerRight() {};
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
+};
+
+
+
+
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === 'ArrowLeft') {
+    moveDodgerLeft();
+  } else if (event.key === 'ArrowRight') {
+    moveDodgerRight();
+  }
+});
